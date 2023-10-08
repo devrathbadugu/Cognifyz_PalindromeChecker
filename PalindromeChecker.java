983bcf16d08d91f760a4e9ce38f14142f2e574be
Devrath Badugu <126484395+devrathbadugu@users.noreply.github.com>
2023-10-08 22:29:29 +0530

    Add files via upload

---
+++ PalindromeChecker.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class PalindromeChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String input = scanner.nextLine();
+        boolean isPalindrome = true;
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - i - 1)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        if (isPalindrome) {
+            System.out.println("The string is a palindrome.");
+        } else {
+            System.out.println("The string is not a palindrome.");
+        }
+        scanner.close();
+    }
+}
